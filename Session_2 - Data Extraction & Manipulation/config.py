2f2a82030edaf0edf1a1db9a3da4887d98752479
miguel <masrazo@gmail.com>
2026-06-15 10:29:59 +0100

    First commit.

---
+++ Session_2 - Data Extraction & Manipulation/config.py
@@ -0,0 +1,35 @@
+##Configurations
+
+pagila_datasets = {
+    
+    "actor": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/actor.csv",
+    "address": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/address.csv",
+    "category": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/category.csv",
+    "city": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/city.csv",
+    "country": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/country.csv",
+    "customer": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/customer.csv",
+    "film_actor": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/film_actor.csv",
+    "film_category": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/film_category.csv",
+    "film": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/film.csv",
+    "inventory": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/inventory.csv",
+    "language": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/language.csv",
+    
+    "payments": {
+        "payment2022_01": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/payment_p2022_01.csv",
+        "payment2022_02": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/payment_p2022_02.csv",
+        "payment2022_03": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/payment_p2022_03.csv",
+        "payment2022_04": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/payment_p2022_04.csv",
+        "payment2022_05": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/payment_p2022_05.csv",
+        "payment2022_06": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/payment_p2022_06.csv",
+        "payment2022_07": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/pagila_csv/payment_p2022_07.csv"
+    }
+}
+
+session_datasets = {
+    "Production.Product": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/Production.Product.csv",
+    "customer": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/customer.csv",
+    "payment": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/payment.csv",
+    "Salaries": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/Salaries.csv"
+}
+
+            
---
+++ Session_3 - Data Quality & Exploratory/config.py
@@ -0,0 +1,9 @@
+#Configurations:
+
+
+session_datasets = {
+    "Production.Product": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/Production.Product.csv",
+    "Salaries": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/Salaries.csv",
+    "train_titanic": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/train_titanic.csv",
+    "superstore": "s3a://rockborne-bucket-01-cbs/Data_Sources_CBS_Risk/superstore.csv"
+}
